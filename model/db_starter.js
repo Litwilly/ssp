3cc82029783a723b3fa3aa0f685cbc018ad43c36
Neliochi <Chinelo.Onuoha@emc.com>
2016-08-27 14:44:35 -0400

    additional tabs for customer tables

--- model/db_starter.js
+++ model/db_starter.js
@@ -1,8 +1,8 @@
 // Bring Mongoose into the project
 var mongoose = require('mongoose');
 // Build the connection string
-var dbURI = 'mongodb://itlp:itlp@ds013486.mlab.com:13486/itlp';
-// var dbURI = 'mongodb://localhost/EMCITLP'; //localhost can be changed to 127.0.0.1 if localhost will not start
+//var dbURI = 'mongodb://itlp:itlp@ds013486.mlab.com:13486/itlp';
+var dbURI = 'mongodb://localhost/EMCITLP'; //localhost can be changed to 127.0.0.1 if localhost will not start
 
 
 // Create the database connection
