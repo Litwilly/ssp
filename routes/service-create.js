38e956fd37e6dc418ab13f4c2853457f9ca3667c
Litwiller, Ryan <litwi1rm@gmail.com>
2016-08-27 14:34:35 -0400

    service order create working

--- routes/service-create.js
+++ routes/service-create.js
@@ -13,6 +13,7 @@ var async = require('async');
 
 exports.create = function(req, res){
   var myequipment = [];
+  var mypriority = [];
   var prods = [];
   var pmTypes = [];
   console.log("==========================Get Serivce Create Fields===============================");
@@ -31,22 +32,30 @@ exports.create = function(req, res){
     },
 
     function(callback){
-      ServiceOrder.find({CloseDate: {$exists: false} })
-        // .where('_CreatedBy').equals(48243)
-        // .populate('_CreatedBy')
-        .populate('_Product')
-        .populate('_Equipment')
+      ProblemType.find()
           //.select('_id SerialNumber OpenDate ProblemTypeDescription ProductName CurrentStatus')
           .exec(function (err, serviceorders){
             serviceorders.forEach(function(yours){
               myequipment.push({
                 "ProblemTypeDescription": yours.ProblemTypeDescription,
+              });
+            });
+            callback();
+          });
+        },
+    function(callback){
+      Priority.find()
+          //.select('_id SerialNumber OpenDate ProblemTypeDescription ProductName CurrentStatus')
+          .exec(function (err, serviceorders){
+            serviceorders.forEach(function(yours){
+              mypriority.push({
                 "PriorityDescription": yours.PriorityDescription
               });
             });
             callback();
           });
         },
+
     function(callback){
       Equipment.find({ _User: req.session.user._id })
           // .populate('_CreatedBy')
@@ -72,67 +81,92 @@ exports.create = function(req, res){
             res.render('service-create',
               { equipments: myequipment,
                 products:  prods,
+                priorities:  mypriority,
                 pmt: pmTypes,
                 firstname: req.session.user.FirstName});
           }
         );
       };
 
+  //Login Functions
 
-  exports.update = function(req, res, callback) {
-    var maxs = Math.floor((Math.random() * 999999) + 720000);
-    var myQuery = new ServiceOrder({
-        _id: maxs,
-        _Equipment: req.body.Equipment.equpid,
-        _CreatedBy: req.session.user._id,
-        ProblemTypeDescription: req.body.ProblemType,
-        PriorityDescription: req.body.Priorty,
-        CustomerContactInfo: {
-          Name: req.body.ContactName,
-          Email: req.body.ContactEmail,
-          Phone: req.body.ContactPhone,
-        },
-        ProblemNotes: req.body.ProblemNotes,
-        OpenDate: req.body.Today,
-        ServiceDetails: {
-          _id: 1,
-          _User: req.session.user._id,
-          StatusDescription: "Submitted By Customer",
-        }
-    });
 
-    myQuery.save(function(err, myQuery) {
-      if (err) return console.error(err);
-      console.dir(myQuery);
-      console.log("works");
-      res.redirect('/customer');
-      if (req.session.RoleName === "Customer") {
+  exports.update = function(req, res) {
+    var maxId;
+    EquipObj = JSON.parse(req.body.Equipment);
+    async.series([
+      function(callback){
+        // find the max service _id
+        ServiceOrder.aggregate( {$group: { _id: "", mymaxid: {$max: "$_id"}}})
+          .exec(function (err, mymax) {
+              var max = mymax[0];
+              maxId = (max.mymaxid + 1);
+              callback(null, 1);
+              console.log(maxId);
+              });
+          },
+        function(callback){
+          if(req.body.PMType === null){
+              ServiceOrder.create({
+                _id: maxId,
+                _Equipment: EquipObj.equipid,
+                _Product: EquipObj.prodid,
+                _CreatedBy: req.session.user._id,
+                ProblemTypeDescription: req.body.ProblemType,
+                PriorityDescription: req.body.Priority,
+                CustomerContactInfo: {
+                  Name: req.body.ContactName,
+                  Email: req.body.ContactEmail,
+                  Phone: req.body.ContactPhone,
+                },
+                ProblemNotes: req.body.ProblemNotes,
+                OpenDate: req.body.Today,
+                // ServiceDetails: {_ID: 1, _User: req.session.user._id, StatusDescription: "Assigned, Waiting to be Accepted" }
+              }, function (err, serviceorder) {
+                callback();
+              });
+            }else {
+              ServiceOrder.create({
+                _id: maxId,
+                _Equipment: EquipObj.equipid,
+                _Product: EquipObj.prodid,
+                _CreatedBy: req.session.user._id,
+                ProblemTypeDescription: req.body.ProblemType,
+                PMDescription: req.body.PMType,
+                PriorityDescription: req.body.Priority,
+                CustomerContactInfo: {
+                  Name: req.body.ContactName,
+                  Email: req.body.ContactEmail,
+                  Phone: req.body.ContactPhone,
+                },
+                ProblemNotes: req.body.ProblemNotes,
+                OpenDate: req.body.Today,
+                // ServiceDetails: {_ID: 1, _User: req.session.user._id, StatusDescription: "Assigned, Waiting to be Accepted" }
+              }, function (err, serviceorder) {
+                callback();
+              });
+            }
+          },
+
+        function(callback){
+          console.log(maxId);
+          ServiceOrder.findOne({ _id: maxId})
+          .exec(function (err, serviceorder){
+            serviceorder.ServiceDetails.push({ _id: 1, _User: req.session.user._id, StatusDescription: "Assigned, Waiting to be Accepted" });
+
+                  // serviceorder.ServiceDetails.push({
+                  //     _id: 1,
+                  //     _User: req.session.user._id,
+                  //     StatusDescription: "Assigned, Waiting to be Accepted"
+                  // });
+                  serviceorder.save(function (err, serviceorder){
+                  });
+            callback();
+          });
+        }], function(err){
+        if (err)return next(err);
+          console.log("works");
           res.redirect('/customer');
-      } else if (req.session.RoleName === "Onsite Engineer") {
-          res.redirect('/engineer');
-      }
-    });
+        });
 
   };
-
-
-  // (function() {
-  //     var info = "Some info";
-  //
-  //     async.parallel([
-  //         function(callback) {
-  //             setTimeout(function() {
-  //                 callback(null, "one");
-  //             }, 200);
-  //         },
-  //         function(callback) {
-  //             setTimeout(function() {
-  //                 callback(null, "two");
-  //             }, 100);
-  //         }
-  //     ],
-  //     function(error, result) {
-  //         console.log(info);
-  //     });
-  //
-  // }) ();
