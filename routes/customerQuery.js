18c2c8252eca2cb83044d954800ffea75e08642d
Neliochi <Chinelo.Onuoha@emc.com>
2016-08-29 11:44:55 -0400

    customer.js work-checkin

--- routes/customerQuery.js
+++ routes/customerQuery.js
@@ -17,6 +17,7 @@ exports.getData = function(req, res){
   var prods = [];
   var completes = [];
   var overdues = [];
+  var checkin = [];
   var td = new Date();
   var ourtoday = td.toUTCString();
   console.log("==Customer Queries==");
@@ -101,6 +102,39 @@ exports.getData = function(req, res){
                 callback();
               });
             },
+        function(callback){
+          ServiceOrder.find()
+            .where(_CreatedBy).equals(req.session.user._id)
+            .where(CurrentStatus).$ne('Completed')
+            .where('Checkin').gt('2016-08-28T09:47:19.000Z')
+            .populate('_CreatedBy')
+            .populate('_Product')
+            .populate('_Equipment')
+            .exec(function (err, serviceorders){
+              serviceorders.forEach(function(yours){
+                checkin.push({
+                  "_id": yours._id,
+                  "_User": yours._User,
+                  "FirstName": yours._User.FirstName,
+                  "StatusDescription": yours._StatusDescription,
+                  "ServiceDetails": yours._ServiceDetails,
+                  "SerialNumber": yours._Equipment.SerialNumber,
+                  "CreatedBy": yours.CreatedBy,
+                  "Room": yours._Equipment.Room,
+                  "InstallDate": yours._Equipment.InstallDate,
+                  "NextPMDate": yours._Equipment.NextPMDate,
+                  "NextPMDescription": yours._Equipment.NextPMDescription,
+                  "OpenDate": yours.OpenDate,
+                  "ProblemTypeDescription": yours.ProblemTypeDescription,
+                  "ProductName": yours._Product.ProductName,
+                  "CurrentStatus": yours.CurrentStatus,
+                  "Checkin": yours._ServiceDetails.Checkin,
+                  "Checkout": yours._ServiceDetails.Checkout,
+                });
+              });
+              callback();
+            });
+        },
         function(callback){
           Equipment.find({ _User: req.session.user._id })
               .populate('_CreatedBy')
